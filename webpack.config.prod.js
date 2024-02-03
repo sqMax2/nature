27e46264e61f41d403974145723dcf1cb68ddbaf
sqMax2 <msvp@mail.ru>
2024-02-03 23:15:43 +0300

    chore: Adds prod optimization plugins

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,30 @@
 const {merge} = require('webpack-merge');
+const TesserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const common = require('./webpack.config.common.js');
 
 
 module.exports = merge(common, {
     mode: 'production',
+	devtool: 'source-map',
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TesserPlugin(),
+			new CssMinimizerPlugin(),
+		],
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+				default: false,
+				vendors: false
+			}
+		}
+	}
     // add your configuration options here
 });
