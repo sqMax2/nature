be4fbee6a2c4252367dc05b423d09b33ec36ad5a
sqMax2 <msvp@mail.ru>
2024-02-03 22:58:29 +0300

    chore: Adds css, sass and font loaders config

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -9,4 +9,19 @@ module.exports = {
 		filename: '[name].[contenthash].js',
 		clean: true,
 	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+			}
+		],
+	}
 };
